1865aeea0a43f60ce179c430430211cc444af406
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-05-15 22:20:49 +0300

    add percentage score

--- index.js
+++ index.js
@@ -1,22 +1,48 @@
-function check () {
-var c= 0;
-var numberOfQuestions =4;
-var q1 = document.quiz.question1.value;
-var q2 = document.quiz.question2.value;
-var q3 = document.quiz.question3.value;
-var q4 = document.quiz.question4.value;
-var result = document.getElementById('result');
-var quiz = document.getElementById('quiz');
-if (q1=="Cascading Style Sheet") {c++}
-if (q2=="HTML") {c++}
-if (q3=="No") {c++}
-if (q4=="js") {c++}
-
- result.textContent = Math.round((c / numberOfQuestions) * 100)
-
-
-
+function check() {
+    var c = 0;
+    var numberOfQuestions = 4;
+    var q1 = document.quiz.question1.value;
+    var q2 = document.quiz.question2.value;
+    var q3 = document.quiz.question3.value;
+    var q4 = document.quiz.question4.value;
+    var result = document.getElementById("result");
+    var quiz = document.getElementById("quiz");
+    if (q1 == "Cascading Style Sheet") {
+        c++
+    }
+    if (q2 == "HTML") {
+        c++
+    }
+    if (q3 == "No") {
+        c++
+    }
+    if (q4 == "js") {
+        c++
+    }
+   
+    
+    result.textContent = Math.round((c / numberOfQuestions) * 100)
+   
+   quiz.style.display = "none";
+   if (c <= 2) {
+    result.textContent = `Your score is ${c}. Retake quiz!`
+    if (c = 3) {
+        result.textContent = `Your score is ${c}. Fair`
+    } else {
+        result.textContent = `Your score is ${c}. Excellent`
+    }
 
 }
 
-
+}
+function score(){
+    var c = 0;
+    var numberOfQuestions = 4;
+    var q1 = document.quiz.question1.value;
+    var q2 = document.quiz.question2.value;
+    var q3 = document.quiz.question3.value;
+    var q4 = document.quiz.question4.value;
+    var result = document.getElementById('result');
+    var quiz = document.getElementById('quiz');
+    
+}
