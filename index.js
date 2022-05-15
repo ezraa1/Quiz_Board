679da865424196adce544a6ba6ca8256b0fa0d60
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-05-15 15:57:09 +0300

    change the baground color to aqua

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ var q1 = document.quiz.question1.value;
 var q2 = document.quiz.question2.value;
 var q3 = document.quiz.question3.value;
 var q4 = document.quiz.question4.value;
+// var result = document.getElementById('result');
+// var quiz = document.getElementById('quiz');
 if (q1=="Cascading Style Sheet") {c++}
 if (q2=="HTML") {c++}
 if (q3=="No") {c++}
 if (q4=="js") {c++}
 document.write(c);
+// quiz.style.display = ""; 
+// if (c<=50) {
+//     result.textContent =`Fail`
+
+// } else {
+//     result.textContent = `Pass`
+// }
 }
 
 
